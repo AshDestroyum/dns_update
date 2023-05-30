3547e12a4d07a7610f959e777846c8455fbeb162
AshDestroyum <77030556+AshDestroyum@users.noreply.github.com>
2023-05-30 07:04:35 +0000

    Add files via upload

--- dns_update.py
+++ dns_update.py
@@ -1,21 +1,68 @@
+# This script was to made to change your DNS in a more efficient manner, always rememmber to run this script under
+# admin privileges for it to work, only for Windows.
+
+
 import subprocess
 import sys
+import ctypes
+import os
+
+# Checking for elevated privileges
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+
+if is_admin():
+    pass
+else:
+    sys.exit("Not running with elevated privileges.")
 
 
-rule = "Get-NetConnectionProfile | Select-Object -ExpandProperty InterfaceAlias"
+# To display current adapter
+def current_conn():
 
-command = subprocess.check_output(["Powershell", "-Command", rule], encoding='utf-8')
+	rule = "Get-NetConnectionProfile | Select-Object -ExpandProperty InterfaceAlias"
 
-print(f"Current connection: {command}")
+	command = subprocess.check_output(["Powershell", "-Command", rule], encoding='utf-8')
 
-adapter = str(command).replace("\n", "")
+	adapter = str(command).replace("\n", "")
 
+	return adapter
+
+# To display current DNS
+def current_dns(connection_name):
+
+	rule2 = f"Get-NetIPInterface -AddressFamily IPv4 -InterfaceAlias '{connection_name}' | Select-Object -ExpandProperty ifIndex"
+
+	command2 = subprocess.check_output(["Powershell", "-Command", rule2], encoding='utf-8')
+
+	index = str(command2).replace("\n", "")
+
+	index_int = int(index)
+
+	rule3 = f"Get-DnsClientServerAddress -InterfaceIndex {index_int} -AddressFamily IPv4 | Select-Object -ExpandProperty ServerAddresses"
+
+	command3 = subprocess.check_output(["Powershell", "-Command", rule3], encoding='utf-8')
+
+	now_dns = str(command3).replace("\n", "")
+
+	return now_dns
+
+print(f"Current connection: {current_conn()}")
+
+print(f"Current DNS: {current_dns(current_conn())}")
+
+
+# DNS input
 dns = input("Enter the DNS for the adapter:\n")
 text = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j',
 'k','l','m','n','o','p','q',
 'r','s','t','u','v','w','x','y',
 'z','!','@','#','$','%','^','&','*','(',')','_','+','{','}','|',':','<','>','?','`','~']
 
+# Checking conidtions for DNS
 if len(dns) > 15:
 	sys.exit("This is not a real DNS...")
 
@@ -26,8 +73,10 @@ for char in dns:
 
 
 
+# Setting the DNS
+rule3 = f"Set-DnsClientServerAddress -InterfaceAlias '{current_conn()}' -ServerAddresses ('{dns}')"
 
-rule2 = f"Set-DnsClientServerAddress -InterfaceAlias '{adapter}' -ServerAddresses ('{dns}')"
+result = subprocess.run(["Powershell", "-Command", rule3])
 
-result = subprocess.run(["Powershell", "-Command", rule2])
+print("DNS change complete!")
 
